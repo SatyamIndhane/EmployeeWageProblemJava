a3ac9c74543f9f564836db78494d0404b80ad342
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-01 16:58:04 +0530

    Usage of Case statement for employee wage

--- EmpAbsentOrPresent.java
+++ EmpAbsentOrPresent.java
@@ -6,8 +6,8 @@ public class EmpAbsentOrPresent
 	{
 	//	Constants
 		
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 		int FULL_DAY_HOUR = 8;
 		int PART_TIME_HOUR = 4;
@@ -15,16 +15,21 @@ public class EmpAbsentOrPresent
 	//	Variables
 		
 		int salary = 0;
-		double empCheck = Math.floor( ( Math.random()*10 )%3 );
+		int empCheck = (int) Math.floor( ( Math.random()*10 )%3 );
 		
 		
-		if (empCheck == IS_FULL_TIME)
-			salary = FULL_DAY_HOUR*WAGE_PER_HOUR;
-		else if (empCheck == IS_PART_TIME)
-			salary = PART_TIME_HOUR*WAGE_PER_HOUR;
+		switch(empCheck)
+		{
+			case IS_FULL_TIME:
+				salary = FULL_DAY_HOUR*WAGE_PER_HOUR;
+				break;
+			case IS_PART_TIME:
+				salary = PART_TIME_HOUR*WAGE_PER_HOUR;
+				break;
+		}
 			
-		System.out.println("Employee's Daily Wage is "+salary);
-	//  System.out.printf("Employee's Daily Wage is %d",salary);
+		System.out.println("Employee's Daily Wage is "+salary);   // This is slow
+	//  System.out.printf("Employee's Daily Wage is %d",salary);     This is fast
 			
 		
 		
