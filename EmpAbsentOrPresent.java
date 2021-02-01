ec1e30285f83a7741f1f078fc2a0bc4e43cafffd
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-01 16:08:17 +0530

    Calculation of Employee's daily wage

--- EmpAbsentOrPresent.java
+++ EmpAbsentOrPresent.java
@@ -5,13 +5,19 @@ public class EmpAbsentOrPresent
 	public static void main(String[] args) 
 	{
 		int IS_FULL_TIME = 1;
+		int WAGE_PER_HOUR = 20;
+		int FULL_DAY_HOUR = 8;
+		int salary = 0;
 		double empCheck = Math.floor( ( Math.random()*10 )%2 );
 		
 		
 		if (empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is absent");
+			salary = FULL_DAY_HOUR*WAGE_PER_HOUR;
+			
+		System.out.println("Employee's Daily Wage is "+salary);
+	//  System.out.printf("Employee's Daily Wage is %d",salary);
+			
+		
 		
 
 	}
