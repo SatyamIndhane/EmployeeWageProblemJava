ef5acc3a591a01f6dcfb67cd449a59b4be1d36ec
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-03 20:43:16 +0530

    Calculating wages till totalworking hours or days is reached for a month

--- EmpAbsentOrPresent.java
+++ EmpAbsentOrPresent.java
@@ -1,6 +1,6 @@
 package empwage;
 
-public class EmpAbsentOrPresent 
+public class EmployeeWageComputationProblem
 {
 	public static void main(String[] args) 
 	{
@@ -9,8 +9,6 @@ public class EmpAbsentOrPresent
 		final int IS_FULL_TIME = 1;
 		final int IS_PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
-		int FULL_DAY_HOUR = 8;
-		int PART_TIME_HOUR = 4;
 		int WORKING_DAY_PER_MONTH = 20;
 		int TOTAL_WORKING_HOUR = 100;
 	
@@ -20,7 +18,6 @@ public class EmpAbsentOrPresent
 		int empHrs=0;
 		int dayCount=0;
 		int hourCount=0;
-		int salary = 0;
 		
 		
 		while ( dayCount<WORKING_DAY_PER_MONTH && hourCount<TOTAL_WORKING_HOUR )
@@ -29,24 +26,21 @@ public class EmpAbsentOrPresent
 			switch(empCheck)
 			{
 				case IS_FULL_TIME:
-					salary = FULL_DAY_HOUR*WAGE_PER_HOUR;
 					empHrs = 8;
 					break;
 				case IS_PART_TIME:
-					salary = PART_TIME_HOUR*WAGE_PER_HOUR;
 					empHrs = 4;
 					break;
 				default:
 					empHrs = 0;
-					salary = 0;
 			}
-			totalSalary = totalSalary + salary;
 			hourCount = hourCount + empHrs;
 			dayCount++;
 		}
 		
-		System.out.println("Employee's Monthly Wage is "+totalSalary);   // This is slow
-	//  System.out.printf("Employee's Monthly Wage is %d",totalSalary);     This is fast
+		totalSalary = hourCount * WAGE_PER_HOUR; 
+		System.out.println("Employee's Monthly Wage is "+totalSalary); 
+	
 			
 		
 		
@@ -54,3 +48,4 @@ public class EmpAbsentOrPresent
 	}
 
 }
+
