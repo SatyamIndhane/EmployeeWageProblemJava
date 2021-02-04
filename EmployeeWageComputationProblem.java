f08b959606ee426ba0fc72f016081d6cf15393d9
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-04 20:52:33 +0530

    Compute Wage For Multiple Companies

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -2,49 +2,69 @@ package empwage;
 
 public class EmployeeWageComputationProblem
 {  
+	// Class Constants
+	public final String company;
+	public final int wagePerHour;
+	public final int workingDayPerMonth;
+	public final int totalWorkingHours;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 	
-	public static void main(String[] args) 
+	// Class Variables
+	public static int totalSalary=0;
+	public static int empHrs=0;
+	public static int dayCount=0;
+	public static int hourCount=0;
+	
+	// Constructor
+	public EmployeeWageComputationProblem(String company, int wagePerHour, int workingDayPerMonth, int totalWorkingHours)
 	{
-		computeEmpWage();
+		this.company = company;
+		this.wagePerHour = wagePerHour;
+		this.workingDayPerMonth = workingDayPerMonth;
+		this.totalWorkingHours = totalWorkingHours;
 	}
 	
-	public static int computeEmpWage()
+	// Main 
+	public static void main(String[] args) 
 	{
-		//	Constants
-			final int IS_FULL_TIME = 1;
-			final int IS_PART_TIME = 2;
-			int WAGE_PER_HOUR = 20;
-			int WORKING_DAY_PER_MONTH = 20;
-			int TOTAL_WORKING_HOUR = 100;
+		// dMart
+		EmployeeWageComputationProblem dMart = new EmployeeWageComputationProblem("dMart", 20, 20, 100);
+		int dmartSalary = wageCal(dMart);
 		
-		//	Variables	
-			int totalSalary=0;
-			int empHrs=0;
-			int dayCount=0;
-			int hourCount=0;
-				
-			while ( dayCount<WORKING_DAY_PER_MONTH && hourCount<TOTAL_WORKING_HOUR )
-			{							
-				int empCheck =  (int) Math.floor( ( Math.random()*10 )%3 );
-				switch(empCheck)
-				{
-					case IS_FULL_TIME:
-						empHrs=8;
-						break;
-					case IS_PART_TIME:
-						empHrs=4;
-						break;
-					default:
-						empHrs=0;
-						break;
-				}
-				hourCount = hourCount + empHrs;
-				dayCount++;
+		// Reliance
+		EmployeeWageComputationProblem reliance = new EmployeeWageComputationProblem("Reliance", 10, 30, 200);
+		int relianceSalary = wageCal(reliance);
+		
+		System.out.println("Dmart's Wage is "+dmartSalary);
+		System.out.println("Reliance's Wage is "+relianceSalary);
+		
+	}
+	
+	// Multiple Companies Wage Calculation Method
+	public static int wageCal(EmployeeWageComputationProblem o)
+	{
+		while ( dayCount<o.workingDayPerMonth && hourCount<o.totalWorkingHours )
+		{							
+			int empCheck =  (int) Math.floor( ( Math.random()*10 )%3 );
+			switch(empCheck)
+			{
+				case IS_FULL_TIME:
+					empHrs=8;
+					break;
+				case IS_PART_TIME:
+					empHrs=4;
+					break;
+				default:
+					empHrs=0;
+					break;
 			}
-			
-			totalSalary = hourCount * WAGE_PER_HOUR; 
-			System.out.println("Employee's Monthly Wage is "+totalSalary);
-			return totalSalary;
+			hourCount = hourCount + empHrs;
+			dayCount++;
+		}
+		
+		totalSalary = hourCount * o.wagePerHour; 
+		return totalSalary;
 	}
 
 }
