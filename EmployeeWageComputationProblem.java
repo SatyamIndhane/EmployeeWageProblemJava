56215594534b2958a36e4397a6008e40a12fce15
SatyamIndhane <satyam.indhane@gmail.com>
2021-02-04 18:01:21 +0530

    Refactored the Code with Class Method For EmpWageComputation

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -1,48 +1,50 @@
 package empwage;
 
 public class EmployeeWageComputationProblem
-{
-	//Class Constants
-	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;  
+{  
 	
 	public static void main(String[] args) 
 	{
-	//	Constants
-		int WAGE_PER_HOUR = 20;
-		int WORKING_DAY_PER_MONTH = 20;
-		int TOTAL_WORKING_HOUR = 100;
-	
-	//	Variables	
-		int totalSalary=0;
-		int empHrs=0;
-		int dayCount=0;
-		int hourCount=0;
-			
-		while ( dayCount<WORKING_DAY_PER_MONTH && hourCount<TOTAL_WORKING_HOUR )
-		{							
-			empHrs = getEmployeeHours();
-			hourCount = hourCount + empHrs;
-			dayCount++;
-		}
-		
-		totalSalary = hourCount * WAGE_PER_HOUR; 
-		System.out.println("Employee's Monthly Wage is "+totalSalary); 
-		
+		computeEmpWage();
 	}
 	
-	public static int getEmployeeHours()
+	public static int computeEmpWage()
 	{
-		int empCheck =  (int) Math.floor( ( Math.random()*10 )%3 );
-		switch(empCheck)
-		{
-			case IS_FULL_TIME:
-				return 8;
-			case IS_PART_TIME:
-				return 4;
-			default:
-				return 0;
-		}
+		//	Constants
+			final int IS_FULL_TIME = 1;
+			final int IS_PART_TIME = 2;
+			int WAGE_PER_HOUR = 20;
+			int WORKING_DAY_PER_MONTH = 20;
+			int TOTAL_WORKING_HOUR = 100;
+		
+		//	Variables	
+			int totalSalary=0;
+			int empHrs=0;
+			int dayCount=0;
+			int hourCount=0;
+				
+			while ( dayCount<WORKING_DAY_PER_MONTH && hourCount<TOTAL_WORKING_HOUR )
+			{							
+				int empCheck =  (int) Math.floor( ( Math.random()*10 )%3 );
+				switch(empCheck)
+				{
+					case IS_FULL_TIME:
+						empHrs=8;
+						break;
+					case IS_PART_TIME:
+						empHrs=4;
+						break;
+					default:
+						empHrs=0;
+						break;
+				}
+				hourCount = hourCount + empHrs;
+				dayCount++;
+			}
+			
+			totalSalary = hourCount * WAGE_PER_HOUR; 
+			System.out.println("Employee's Monthly Wage is "+totalSalary);
+			return totalSalary;
 	}
 
 }
